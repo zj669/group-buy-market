9fa452f4e88eab2f95d248710f8e3e607961b27d
JieZhou <jiezhou@ddmarketinghub.com>
2025-08-14 16:39:40 +0800

    feat:增加拼团订单的mapper层

---
+++ group-buy-market-infrastructure/src/main/java/com/zj/infrastructure/dao/IGroupBuyOrderDao.java
@@ -0,0 +1,23 @@
+package com.zj.infrastructure.dao;
+
+
+import com.zj.infrastructure.dao.po.GroupBuyOrder;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author Fuzhengwei bugstack.cn @小傅哥
+ * @description 用户拼单
+ * @create 2025-01-11 10:33
+ */
+@Mapper
+public interface IGroupBuyOrderDao {
+
+    void insert(GroupBuyOrder groupBuyOrder);
+
+    int updateAddLockCount(String teamId);
+
+    int updateSubtractionLockCount(String teamId);
+
+    GroupBuyOrder queryGroupBuyProgress(String teamId);
+
+}
---
+++ group-buy-market-infrastructure/src/main/java/com/zj/infrastructure/dao/IGroupBuyOrderListDao.java
@@ -0,0 +1,19 @@
+package com.zj.infrastructure.dao;
+
+
+import com.zj.infrastructure.dao.po.GroupBuyOrderList;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author Fuzhengwei bugstack.cn @小傅哥
+ * @description 用户拼单明细
+ * @create 2025-01-11 09:07
+ */
+@Mapper
+public interface IGroupBuyOrderListDao {
+
+    void insert(GroupBuyOrderList groupBuyOrderListReq);
+
+    GroupBuyOrderList queryGroupBuyOrderRecordByOutTradeNo(GroupBuyOrderList groupBuyOrderListReq);
+
+}
